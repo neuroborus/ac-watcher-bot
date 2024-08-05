63b1307050a5d47727cd33efa076bf16cc96ee2f
neuroborus <mihasso@protonmail.com>
2024-08-05 12:50:31 +0300

    update time tool

--- src/tools/time.js
+++ src/tools/time.js
@@ -11,7 +11,7 @@ function minimizeDate(date) {
     newDate.setHours(0);
     newDate.setMinutes(0);
     newDate.setSeconds(0);
-    newDate.setMilliseconds(1);
+    newDate.setMilliseconds(0);
     return newDate;
 }
 
